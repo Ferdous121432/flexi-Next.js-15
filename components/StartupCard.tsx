f5c126a335c702689905fcb029fe4a283191a204
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2025-03-03 04:09:03 +0600

    Image

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -53,7 +53,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       <Link href={`/startup/${_id}`}>
         <p className="startup-card_desc">{description}</p>
 
-        <img src={image} alt="placeholder" className="startup-card_img" />
+        <Image src={image} alt="placeholder" className="startup-card_img" />
       </Link>
 
       <div className="flex-between gap-3 mt-5">
